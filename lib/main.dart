603c7902c7aca01aa79140d91b355c9cbc5f20d4
AlaaTaha <alaat7058@gmail.com>
2025-04-13 18:37:12 -0700

    ayattf

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,6 @@ import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-  home: Scaffold(
-    body: Center(child: Text("Ayat")),
-  ),
-  ));
+  runApp(MyApp());
 }
 
